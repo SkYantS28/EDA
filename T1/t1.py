b776a08f9476bd136cb3364aaa6cb96b098484d6
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-10 00:17:35 -0300

    equações

--- T1/t1.py
+++ T1/t1.py
@@ -24,3 +24,10 @@ print(f'O ponto de interseção é: x = {x_intersecao}, y = {y_intersecao}')
 
 # desenhar grafico equação
 x = np.linspace(-1, 4, 100)
+
+# equações
+y1 = 0.5 * x +0.5
+y2 = -x + 2
+plt.figure(figsize=(8, 6))
+plt.plot(x, y1, label='y = 0.5x + 0.5'. color='blue')
+plt.plot(x, y2, label='y = -x + 2', color='red')
