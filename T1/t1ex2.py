ca6625df43f2275ca43d4d048e497185211c9a21
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-10 00:33:52 -0300

    arquivo ex2 + enunciado

---
+++ T1/t1ex2.py
@@ -0,0 +1,13 @@
+'''
+Exercício 02
+
+    O problema que usaremos é bem conhecido e bastante simples.
+    
+    Queremos resolver uma equação de segundo grau, ou seja, dada a equação :
+
+        𝒂𝒙𝟐 + 𝒃𝒙 + 𝒄,
+
+    queremos saber quais são as suas raízes reais, se elas existirem.
+    
+    Desta forma, resolva a função 2𝑥ˆ2 + 2𝑥 − 6.
+'''
