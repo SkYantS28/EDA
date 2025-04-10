1eefe4bee9327f18192247d6c7e20c1d74b312fd
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-10 00:25:51 -0300

    linha 32 alteração
    em vez de ',' utilizei '.'

--- T1/t1.py
+++ T1/t1.py
@@ -29,7 +29,7 @@ x = np.linspace(-1, 4, 100)
 y1 = 0.5 * x +0.5
 y2 = -x + 2
 plt.figure(figsize=(8, 6))
-plt.plot(x, y1, label='y = 0.5x + 0.5'. color='blue')
+plt.plot(x, y1, label='y = 0.5x + 0.5', color='blue')
 plt.plot(x, y2, label='y = -x + 2', color='red')
 
 # ponto interseção
